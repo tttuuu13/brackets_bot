6bd2d8f384205b3a6e650c3ee6b8a85aa2784fbd
tttuuu13 <56789432+tttuuu13@users.noreply.github.com>
2021-11-25 23:11:31 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -500,22 +500,23 @@ def answer(message):
 
 @bot.message_handler(func=lambda m: True, content_types=['text'])
 def create_word(message):
-    #try:
-    for i in message.text.split():
-        photo, new_line = word(i, str(message.chat.id))
-        if not '?' in i:
-            photo.save(i + '.png')
-            photo = open(i + '.png', 'rb')
-        else:
-            i = 'poop'
-            photo.save(i + '.png')
-            photo = open(i + '.png', 'rb')
-        bot.send_document(chat_id=message.chat.id, data=photo, caption=i)
-        photo.close()
-        os.remove(i + '.png')
-        bot.send_message(chat_id=599040955, text=i)
-    #except:
-        #bot.send_message(chat_id=message.chat.id, text='Произошла ошибка, проверьте отсутствие недопустимых знаков знаков')
+    try:
+        for i in message.text.split():
+            photo, new_line = word(i, str(message.chat.id))
+            if not '?' in i:
+                photo.save(i + '.png')
+                photo = open(i + '.png', 'rb')
+            else:
+                i = 'poop'
+                photo.save(i + '.png')
+                photo = open(i + '.png', 'rb')
+            bot.send_document(chat_id=message.chat.id, data=photo, caption=i)
+            photo.close()
+            os.remove(i + '.png')
+            bot.send_message(chat_id=599040955, text=i)
+    except:
+        bot.send_message(chat_id=message.chat.id, text='Произошла ошибка, проверьте отсутствие недопустимых знаков знаков')
+        print('word error')
 
 @bot.callback_query_handler(lambda query: query.data == 'Выход')
 def exit_func(query):
