3668f5a9397eebf1afabbfeb831623c925b08d18
tttuuu13 <56789432+tttuuu13@users.noreply.github.com>
2022-08-03 03:06:52 +0500

    Update words.py

--- words.py
+++ words.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import math
-
+from admin import get_info
 
 
 a = ['а', 'е', 'ё', 'и', 'о', 'у', 'э', 'ю', 'я', 'ы', 'і', 'ї', 'є']
@@ -9,7 +9,7 @@ s_drop = Image.open('звезда.png')
 s_drop = s_drop.resize((62, 62))
 b_drop = Image.open('звезда.png')
 b_drop = b_drop.resize((126, 126))
-boat_1 = Image.open('самолет1.png')
+boat_1 = Image.open('самолет1.png').convert("RGBA")
 
 def word(word, id):
     global boat_1
@@ -19,11 +19,21 @@ def word(word, id):
     chars = []
     new_line = False
     
-    try:
-        with open("preferences.txt", "r", encoding='utf-8') as f:
-            excluded = eval(f.read())[id]['wordType']
-    except:
-        excluded = ""
+    info = get_info(id)
+    excluded = info[2]
+    if info[4]:
+        word = word.upper()
+        boat_y = 580
+        drop_y = 580
+        accent_y = 160
+        boat_height = 250
+        blank = 250
+    else:
+        boat_y = 610
+        drop_y = 610
+        accent_y = 230
+        boat_height = 200
+        blank = 200
         
     
     for char in word:
@@ -31,7 +41,7 @@ def word(word, id):
             if char.lower() in excluded:
                 img = Image.open('Буквы/blank.png')
                 width, height = img.size
-                img = img.resize((250, 20), Image.ANTIALIAS)
+                img = img.resize((blank, 120), Image.ANTIALIAS)
                 width, height = img.size
                 width_total += width
                 widths.append(width)
@@ -52,7 +62,10 @@ def word(word, id):
             elif char == '*':
                 img = Image.open("Буквы/'.png")
             else:
-                img = Image.open('Буквы/' + char.upper() + '.png')
+                if char == char.upper():
+                    img = Image.open('Буквы/' + char + '.png')
+                else:
+                    img = Image.open('Буквы/' + char * 2 + '.png')
             width, height = img.size
         except:
             pass
@@ -174,14 +187,14 @@ def word(word, id):
                 continue
         else:
             if char == 'ё':
-                img = img.resize((260 * width // height, 260), Image.ANTIALIAS)
+                img = img.resize((270 * width // height, 270), Image.ANTIALIAS)
                 width, height = img.size
                 width_total += width
                 widths.append(width)
                 chars.append(img)
                 continue
             elif char == 'щ' or char == 'ц':
-                img = img.resize((200 * width // height, 220), Image.ANTIALIAS)
+                img = img.resize((235 * width // height, 235), Image.ANTIALIAS)
                 width, height = img.size
                 width_total += width
                 widths.append(width)
@@ -194,8 +207,43 @@ def word(word, id):
                 widths.append(width)
                 chars.append(img)
                 continue
+            elif char == 'д':
+                img = img.resize((230 * width // height, 230), Image.ANTIALIAS)
+                width, height = img.size
+                width_total += width
+                widths.append(width)
+                chars.append(img)
+                continue
+            elif char == 'б':
+                img = img.resize((280 * width // height, 280), Image.ANTIALIAS)
+                width, height = img.size
+                width_total += width
+                widths.append(width)
+                chars.append(img)
+                continue
             elif char == 'й':
-                img = img.resize((250 * width // height, 250), Image.ANTIALIAS)
+                img = img.resize((285 * width // height, 285), Image.ANTIALIAS)
+                width, height = img.size
+                width_total += width
+                widths.append(width)
+                chars.append(img)
+                continue
+            elif char == 'р':
+                img = img.resize((270 * width // height, 270), Image.ANTIALIAS)
+                width, height = img.size
+                width_total += width
+                widths.append(width)
+                chars.append(img)
+                continue
+            elif char == 'у':
+                img = img.resize((280 * width // height, 280), Image.ANTIALIAS)
+                width, height = img.size
+                width_total += width
+                widths.append(width)
+                chars.append(img)
+                continue
+            elif char == 'ф':
+                img = img.resize((330 * width // height, 330), Image.ANTIALIAS)
                 width, height = img.size
                 width_total += width
                 widths.append(width)
@@ -223,58 +271,63 @@ def word(word, id):
                 chars.append(img)
                 continue
 
-    bg = Image.new('RGB', (width_total, 600), 'white')
+    bg = Image.new('RGB', (width_total, 800), 'white')
     x = 40
     index = 0
     for img in chars:
         if word[index] == 'Ё':
-            bg.paste(img, (x, 20))
+            bg.paste(img, (x, 220))
         elif word[index] == 'ё':
-            bg.paste(img, (x, 80))
+            bg.paste(img, (x, 265))
+        elif word[index] == 'б':
+            bg.paste(img, (x, 265))
         elif word[index] == 'Й':
-            bg.paste(img, (x, 20))
+            bg.paste(img, (x, 220))
         elif word[index] == 'й':
-            bg.paste(img, (x, 90))
+            bg.paste(img, (x, 255))
+        elif word[index] == 'ф':
+            bg.paste(img, (x, 275))
         elif word[index] == '.':
-            bg.paste(img, (x, 290))
-        elif word[index] in excluded:
-            bg.paste(img, (x, 330))
+            bg.paste(img, (x, 490))
+        elif word[index] in excluded or word[index].lower() in excluded:
+            bg.paste(img, (x, 490))
         elif word[index] == "*":
-            bg.paste(img, (x - widths[index-1] // 2 - 74, 30))
+            bg.paste(img, (x - widths[index-1] // 2 - 74, accent_y))
         elif word[index] == ',':
-            bg.paste(img, (x, 310))
+            bg.paste(img, (x, 510))
         elif word[index] == ',':
-            bg.paste(img, (x, 50))
+            bg.paste(img, (x, 250))
         elif word[index] == '!':
-            bg.paste(img, (x, 50))
+            bg.paste(img, (x, 250))
         elif word[index] == '?':
-            bg.paste(img, (x, 50))
+            bg.paste(img, (x, 250))
         elif word[index] == ':':
-            bg.paste(img, (x, 140))
+            bg.paste(img, (x, 340))
         elif word[index] == ';':
-            bg.paste(img, (x, 140))
+            bg.paste(img, (x, 340))
         elif word[index] == 'Ґ':
-            bg.paste(img, (x, 45))
+            bg.paste(img, (x, 245))
         elif word[index] == 'ґ':
-            bg.paste(img, (x, 110))
+            bg.paste(img, (x, 310))
         elif word[index] == 'Ї':
-            bg.paste(img, (x, 10))
+            bg.paste(img, (x, 210))
         elif word[index] == 'ї':
-            bg.paste(img, (x, 80))
+            bg.paste(img, (x, 280))
         elif word[index] == 'і' or word[index] == 'i':
-            bg.paste(img, (x, 60))
+            bg.paste(img, (x, 260))
         elif word[index] == '-' or word[index] == '–' or word[index] == '—':
-            bg.paste(img, (x, 190))
+            bg.paste(img, (x, 390))
         elif word[index] == word[index].upper():
-            bg.paste(img, (x, 80))
+            bg.paste(img, (x, 280))
         elif word[index] == word[index].lower():
-            bg.paste(img, (x, 140))
+            bg.paste(img, (x, 340))
         x += widths[index] + 40
         index += 1
-    drop_y = 370
-    boat_y = 370
     
+    if not get_info(id)[3]:
+        bg = bg.convert('L').point(lambda x: 255 if x > 200 else 0)
     
+
     index = 0
     while index < len(word):
         if word[index].lower() in a:
@@ -290,8 +343,8 @@ def word(word, id):
                 elif word[index + 1].lower() in a:
                     boat_x = sum(widths[:index]) + 40 * len(widths[:index]) + 40 + 20
                     b_width, b_height = boat.size
-                    boat_place = boat.resize((40 + widths[index] + widths[index+1] - 40, 150))
-                    bg.paste(boat_place, (boat_x, boat_y))
+                    boat_place = boat.resize((40 + widths[index] + widths[index+1] - 40, boat_height))
+                    bg.paste(boat_place, (boat_x, boat_y), boat_place)
                     index += 2
                 else:
                     s_drop_x = sum(widths[:index]) + 40 * len(widths[:index]) + 40 + widths[index] // 2 - 31
@@ -303,6 +356,7 @@ def word(word, id):
                 index += 1
         else:
             index += 1
+
     return bg, new_line
 
 
